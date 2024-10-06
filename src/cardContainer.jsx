b7cf62569416460287786a8fcd0f6cb645c36809
Paras_Raina <parasr243@gmail.com>
2024-10-06 17:12:41 +0530

    updated code

--- src/cardContainer.jsx
+++ src/cardContainer.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from "react";
+/*import { useState,useEffect } from "react";
 import CardImage from "./cardImage"
 const CardContainer = () => {
 //const endpoint = "https://restcountries.com/v3.1/all";
@@ -40,7 +40,7 @@ async(){
   
     fetchData();
   }, []); 
-  */useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
         try {
           const response = await fetch("https://restcountries.com/v3.1/all");
@@ -93,5 +93,82 @@ async(){
   );
   
 }
+
+export default CardContainer;*/
+import { useState, useEffect } from "react";
+import CardImage from "./cardImage";
+
+const CardContainer = () => {
+  const [countries, setCountries] = useState([]); // To store country data
+  const [searchTerm, setSearchTerm] = useState(''); // To store search input
+
+  // Fetch country data from the API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        if (response.ok) {
+          const data = await response.json();
+          setCountries(data); // Set country data to state
+        } else {
+          console.error("Failed to fetch countries. Status:", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+  // Filter countries based on the search term
+  const filteredCountries = countries.filter((country) => 
+    country?.name?.common?.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      padding: "50px",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      
+      {/* Search Input */}
+      <input
+        type="text"
+        className="search-input"
+        style={{ width: "500px", height: "20px" }}
+        value={searchTerm}
+        placeholder="Search for Countries"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {/* Country Cards */}
+      <div className="countryCard" style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px",
+      }}>
+        {filteredCountries.length > 0 ? (
+          filteredCountries.map((country) => (
+            <CardImage
+              key={country.ccn3}
+              name={country.name.common}
+              image={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+            />
+          ))
+        ) : (
+          <p>No matching countries found</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default CardContainer;
--- src/cardImage.jsx
+++ src/cardImage.jsx
@@ -1,4 +1,4 @@
-function CardImage(
+/*function CardImage(
     {name , alt,image,key}
 ) {
     return (
@@ -32,6 +32,33 @@ function CardImage(
         
         </div>
     )
-}
+}*/
+function CardImage({ name, image, alt }) {
+    return (
+      <div style={{
+        padding: "10px",
+      }}>
+        <div className="countryCard"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "150px",
+            height: "200px",
+            borderRadius: "5px",
+            border: "1px solid black",
+            padding: "5px",
+          }}>
+          <img 
+            src={image}
+            alt={alt}
+            style={{ height: "100px", width: "100px" }}
+          />
+          <p>{name}</p>
+        </div>
+      </div>
+    );
+  }
 
 export default CardImage;
