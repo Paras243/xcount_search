59450687d88b4cd7878d916a7bde9eea6d7fce08
Paras_Raina <parasr243@gmail.com>
2024-10-14 16:07:09 +0530

    updated code

--- src/cardContainer.jsx
+++ src/cardContainer.jsx
@@ -95,6 +95,82 @@ async(){
 }
 
 export default CardContainer;*/
+/*import { useState, useEffect } from "react";
+import CardImage from "./cardImage";
+
+const CardContainer = () => {
+  const [countries, setCountries] = useState([]); // To store country data
+  const [searchTerm, setSearchTerm] = useState(''); // To store search input
+
+  // Fetch country data from the API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        if (response.ok) {
+          const data = await response.json();
+          setCountries(data); // Set country data to state
+        } else {
+          console.error("Failed to fetch countries. Status:", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+  // Filter countries based on the search term
+  const filteredCountries = countries.filter((country) => 
+    country?.name?.common?.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      padding: "50px",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      
+      
+      <input
+        type="text"
+        className="search-input"
+        style={{ width: "500px", height: "20px" }}
+        value={searchTerm}
+        placeholder="Search for countries..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      <div className="countryCard" style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px",
+      }}>
+        {filteredCountries.length > 0 ? (
+          filteredCountries.map((country) => (
+            <CardImage
+              key={country.ccn3}
+              name={country.name.common}
+              image={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+            />
+          ))
+        ) : (
+          <p></p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CardContainer;*/
 import { useState, useEffect } from "react";
 import CardImage from "./cardImage";
 
@@ -135,7 +211,6 @@ const CardContainer = () => {
       alignItems: "center",
     }}>
       
-      {/* Search Input */}
       <input
         type="text"
         className="search-input"
@@ -145,30 +220,30 @@ const CardContainer = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
       />
 
-      {/* Country Cards */}
-      <div className="countryCard" style={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "10px",
-      }}>
-        {filteredCountries.length > 0 ? (
-          filteredCountries.map((country) => (
+      {filteredCountries.length > 0 ? (
+        <div style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "10px",
+        }}>
+          {filteredCountries.map((country) => (
             <CardImage
               key={country.ccn3}
               name={country.name.common}
               image={country.flags.png}
               alt={`Flag of ${country.name.common}`}
             />
-          ))
-        ) : (
-          <p></p>
-        )}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <p></p>
+      )}
     </div>
   );
 };
 
 export default CardContainer;
+
